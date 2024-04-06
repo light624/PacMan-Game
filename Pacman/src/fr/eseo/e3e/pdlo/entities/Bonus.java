8986d22872bd974056bd264c66b45ef9a0453e38
Jireh <jireh@gmail.com>
2024-04-06 13:51:44 +0200

    Update

--- Pacman/src/fr/eseo/e3e/pdlo/entities/Bonus.java
+++ Pacman/src/fr/eseo/e3e/pdlo/entities/Bonus.java
@@ -1,23 +1,23 @@
 package fr.eseo.e3e.pdlo.entities;
 
+import fr.eseo.e3e.pdlo.utils.Direction;
+
 public class Bonus extends Entity {
 
 	private boolean collected;
 
-    public Bonus(int x, int y) {
-        super(x, y);
-        this.collected = false;
-    }
+	public Bonus(int x, int y) {
+		super(x, y);
+		this.collected = false;
+	}
 
-   
-    public void collect() {
-        this.collected = true;
-        
-    }
+	public void collect() {
+		this.collected = true;
+
+	}
 
 	@Override
-	public void move() {
-		
-		
+	public void move(Direction direction) {
+
 	}
 }
