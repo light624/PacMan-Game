31b0e5c15f8fcba5dc2c6fe3f7d5635ff7cad32f
Jireh <jireh@gmail.com>
2024-04-06 13:45:53 +0200

    Update Ghost

--- Pacman/src/fr/eseo/e3e/pdlo/entities/Ghost.java
+++ Pacman/src/fr/eseo/e3e/pdlo/entities/Ghost.java
@@ -3,18 +3,20 @@
  */
 package fr.eseo.e3e.pdlo.entities;
 
+import fr.eseo.e3e.pdlo.utils.Direction;
+
 /**
  * 
  */
 public class Ghost extends Entity {
 
 	public Ghost(int x, int y) {
-        super(x, y);
-    }
+		super(x, y);
+	}
 
-    @Override
-    public void move() {
-        
-    }
+	@Override
+	public void move(Direction direction) {
+
+	}
 
 }
