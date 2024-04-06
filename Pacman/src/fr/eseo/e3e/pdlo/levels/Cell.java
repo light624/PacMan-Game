fb875bf6a665bfd4cf3af6ffbbe651a55c392f07
Jireh <jireh@gmail.com>
2024-04-06 15:11:04 +0200

    Update

--- Pacman/src/fr/eseo/e3e/pdlo/levels/Cell.java
+++ Pacman/src/fr/eseo/e3e/pdlo/levels/Cell.java
@@ -3,6 +3,8 @@
  */
 package fr.eseo.e3e.pdlo.levels;
 
+import fr.eseo.e3e.pdlo.utils.CellType;
+
 /**
  * 
  */
@@ -10,12 +12,16 @@ public class Cell {
     private boolean isWall;
     private boolean hasBonus;
     private boolean hasGhost;
+    private CellType type;
     
 
     public Cell(boolean isWall) {
         this.isWall = isWall;
         this.hasBonus = false;
         this.hasGhost = false;
+    }
+    public boolean isWalkable() {
+        return this.type == CellType.PATH || this.type == CellType.BONUS || this.type == CellType.SUPER_BONUS;
     }
 
     
@@ -25,6 +31,13 @@ public class Cell {
 
     public void setWall(boolean isWall) {
         this.isWall = isWall;
+    }
+    public CellType getType() {
+        return type;
+    }
+
+    public void setType(CellType type) {
+        this.type = type;
     }
 
     public boolean hasBonus() {
